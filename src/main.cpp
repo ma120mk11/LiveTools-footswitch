e14fbc7244f62c1511abe39eced25837711c5dc3
Ove <ove.niemisto@gmail.com>
2022-07-05 18:53:11 +0300

    Working with 4 buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 
+#ifndef LOGGING
+    #define LOGGING
+#endif
+
 #define _WEBSOCKETS_LOGLEVEL_ 5
 
 #include <Arduino.h>
@@ -10,7 +14,7 @@
 #include <WebSocketClient.h>
 #include "secrets.h"
 
-#define NBR_OF_BUTTONS 2
+#define NBR_OF_BUTTONS 4
 
 // Modes
 #define WEBSOCKET 1
@@ -18,38 +22,113 @@
 
 #define MODE API
 
-class Button
-{
-public:
-    Button()
-    {
-    }
-    String id = "test";
+String FS_ID = "arduino-test";
+
+
+class Button {
+    public:
+        Button() {}
+        String id = "test";
+        bool isTriggeredInterrupt = false;
 };
 
 /////// Wifi Settings ///////
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
-// char serverAddress[] = "192.168.43.249:8000";  // server address
-char serverAddress[] = "192.168.43.249"; // server address
+char serverAddress[] = "192.168.43.249";    // server address
 int port = 8000;
 
 WiFiClient wifi;
-WebSocketClient socket = WebSocketClient(wifi, serverAddress, port);
+// WebSocketClient socket = WebSocketClient(wifi, serverAddress, port);
 HttpClient client = HttpClient(wifi, serverAddress, port);
 
 int status = WL_IDLE_STATUS;
 unsigned long lastMillis = 0;
 
-const int buttonPin = 4;
+
+
 const int ledPin = LED_BUILTIN;
 
-bool prevBtnValue = 0;
+
+#define pending true
+#define switched true
+#define TRIGGER_TYPE CHANGE
+#define debounce 10
 
 bool buttonValues[NBR_OF_BUTTONS] = {};
+bool initComplete = false;
 
-String FS_ID = "arduino-test";
+volatile bool interruptStatus[NBR_OF_BUTTONS] = {!pending, !pending, !pending, !pending};
+bool prevBtnValue[NBR_OF_BUTTONS] = {0, 0, 0, 0};
+const int btnPin[] = {2, 3, 9, 10};
+
+
+bool switching_pending[NBR_OF_BUTTONS] = {false, false, false, false};
+long int elapse_timer[NBR_OF_BUTTONS];
+
+void printLine() {
+    Serial.println("*************************************************");
+}
+
+void interruptHandler(int buttonId) {
+    if(initComplete) {
+        // Serial.println("Btn " + String(buttonId+1));
+        if (interruptStatus[buttonId] == !pending) {
+            interruptStatus[buttonId] = pending;
+
+            // if (digitalRead(btnPin[buttonId]) == LOW) {
+            // }
+        }
+    }
+}
+
+
+void btn1InterruptHandler() {
+    digitalWrite(ledPin, digitalRead(btnPin[0]));
+    // Serial.println("Btn 1");
+    interruptHandler(0);
+}
+void btn2InterruptHandler() {
+    // Serial.println("Btn 2");
+
+    interruptHandler(1);
+}
+void btn3InterruptHandler() {
+    interruptHandler(2);
+}
+void btn4InterruptHandler() {
+    interruptHandler(3);
+}
+
+
+bool readButton(int id, int btnReading) {
+
+    if (interruptStatus[id] == pending)
+    {
+        // interrupt has been raised on this button so now need to complete
+        // the button read process, ie wait until it has been released
+        // and debounce time elapsed
+        if (btnReading == HIGH)
+        {
+            // switch is pressed, so start/restart wait for button relealse, plus end of debounce process
+            switching_pending[id] = true;
+            elapse_timer[id] = millis(); // start elapse timing for debounce checking
+        }
+        if (switching_pending && btnReading == LOW)
+        {
+            // switch was pressed, now released, so check if debounce time elapsed
+            if (millis() - elapse_timer[id] >= debounce)
+            {
+                // dounce time elapsed, so switch press cycle complete
+                switching_pending[id] = false;         // reset for next button press interrupt cycle
+                interruptStatus[id] = !pending;        // reopen ISR for business now button on/off/debounce cycle complete
+                return switched;                       // advise that switch has been pressed
+            }
+        }
+    }
+    return !switched; // either no press request or debounce period not elapsed
+} // end of readButton function
 
 void printWifiVersion()
 {
@@ -78,12 +157,11 @@ void printWifiVersion()
         Serial.println("   version required by the library, you may experience");
         Serial.println("   issues or failures.");
     }
+    printLine();
 }
 
 void printWiFiStatus()
 {
-    // Prints WiFI status to serial monitor
-
     Serial.print("SSID: ");
     Serial.println(WiFi.SSID());
 
@@ -92,6 +170,7 @@ void printWiFiStatus()
     Serial.print("IP Address: ");
     Serial.println(ip);
     Serial.println("Signal strenght: " + String(WiFi.RSSI()));
+    printLine();
 }
 
 void connectWiFi()
@@ -104,14 +183,11 @@ void connectWiFi()
     }
     WiFi.noLowPowerMode();
     WiFi.setTimeout(10000);
-    // WiFi.beginAP
-    // WiFi.config():
     Serial.print("Connecting to ");
     Serial.println(ssid); // print the network name (SSID);
     while (status != WL_CONNECTED)
     {
         // Connect to WPA/WPA2 network:
-        // status = WiFi.begin(ssid, pass);
         status = WiFi.begin(ssid, pass);
     }
 }
@@ -119,24 +195,37 @@ void connectWiFi()
 void setup()
 {
     Serial.begin(38400);
-    // Serial.begin(9600);
     while (!Serial)
     {
         ; // Wait for serial port to connect
     }
-
-    Serial.println("Starting setup... Mode: " + MODE);
+    printLine();
+    Serial.println("** Starting setup... Mode: " + String(MODE));
 
     pinMode(ledPin, OUTPUT);          // use the LED as an output
-    pinMode(buttonPin, INPUT_PULLUP); // use button pin as an input
+    digitalWrite(ledPin, LOW);
+
+    for (int i = 0; i < NBR_OF_BUTTONS; i++)
+    {
+        pinMode(btnPin[i], INPUT_PULLUP);
+    }
+
+    attachInterrupt(digitalPinToInterrupt(btnPin[0]), btn1InterruptHandler, TRIGGER_TYPE);
+    attachInterrupt(digitalPinToInterrupt(btnPin[1]), btn2InterruptHandler, TRIGGER_TYPE);
+    attachInterrupt(digitalPinToInterrupt(btnPin[2]), btn3InterruptHandler, TRIGGER_TYPE);
+    attachInterrupt(digitalPinToInterrupt(btnPin[3]), btn4InterruptHandler, TRIGGER_TYPE);
 
     printWifiVersion();
     connectWiFi();
     printWiFiStatus();
+
+    initComplete = true;
+    printLine();
 }
 
+
 void connectWebsocket()
-{
+{   /*
     Serial.println("Starting WebSocket client...");
     socket.connectionKeepAlive();
     socket.begin("/footswitch/arduino-test");
@@ -149,7 +238,20 @@ void connectWebsocket()
         socket.print("{\"type\":\"config\",\"data\":{\"fs_id\":\"" + FS_ID + "\"}}");
         socket.endMessage();
     }
+    */
 }
+void postBtnPress(int buttonId, int btnValue ) {
+    String body = "{\"fs_id\":\"" + FS_ID + "\",\"btn_id\":" + String(buttonId) + ",\"state\":" + String(btnValue) + "}";
+    Serial.println(body);
+
+    String contentType = "application/json";
+    client.post("/footswitch/btn-change", contentType, body);
+    int statusCode = client.responseStatusCode();
+    String response = client.responseBody();
+    // Serial.println("Response: " + response);
+    Serial.println("Status code: " + String(statusCode));
+}
+
 
 void loop()
 {
@@ -162,15 +264,15 @@ void loop()
         Serial.println("Wifi: " + wifi.available());
     }
 
-    if (millis() - lastMillis > 5000) {
-        Serial.println("Signal strenght: " + String(WiFi.RSSI()));
+    if (millis() - lastMillis > 10000) {
+        // Serial.println("Signal strenght: " + String(WiFi.RSSI()));
         lastMillis = millis();
     }
 
     if (MODE == WEBSOCKET) {
 
         connectWebsocket();
-
+        /*
         while (socket.connected())
         {
             // Serial.println("WiFi status: " + String(WiFi.status()));
@@ -202,23 +304,20 @@ void loop()
             // delay(100);
         }
         Serial.println("disconnected");
+        */
     }
 
     else if (MODE == API) {
-        bool buttonValue = !digitalRead(buttonPin);
-        if (buttonValue != prevBtnValue)
-        {
-            String body = "{\"fs_id\":\"" + FS_ID + "\",\"btn_id\":0,\"state\":" + String(buttonValue) + "}";
-            Serial.println(body);
-
-            String contentType = "application/json";
-            client.post("/footswitch/btn-change", contentType, body);
-            int statusCode = client.responseStatusCode();
-            String response = client.responseBody();
-            Serial.println("Status code: " + String(statusCode));
-            Serial.println("Response: " + response);
+        for (int id = 0; id < NBR_OF_BUTTONS; id++) {
+            bool buttonValue = !digitalRead(btnPin[id]);
+            if (readButton(id, buttonValue) == switched)
+            {
+                Serial.println("Button " + String(id) + " value: " + String(buttonValue));
+                postBtnPress(id, !buttonValue);
+            }
         }
-        prevBtnValue = buttonValue;
     }
 
+    // Serial.println("--> " + String(digitalRead(btnPin[0])) + " - " + String(digitalRead(btnPin[1])) + " - " + String(digitalRead(btnPin[2])) + " - " + String(digitalRead(btnPin[3])));
+    // delay(50);
 }
